2bffe4c6c6b35429d226f7b24cf8a6f4e473cf70
majo48 <mart.jonasse@gmail.com>
2020-10-31 17:27:32 +0100

    Added exifread library Read meta data from jpeg file and print creation date (original).

--- recreate/common/myfile.py
+++ recreate/common/myfile.py
@@ -1,5 +1,5 @@
 """ MyFile: set file datetime created to metadata.creation_time """
-import io, sys, os, filetype
+import io, sys, os, filetype, exifread
 
 HEADERLEN = 262
 
@@ -14,8 +14,33 @@ class MyFile:
         self.bytes = self._get_header_bytes(file_path)
         self.mime = self._get_mime(self.bytes)
         self.extension = os.path.splitext(file_path)[1].lower()
-        self.output = {'path': self.path, 'mime': self.mime}
-        # finished
+        self.tags = self._get_exif_tags(file_path)
+        # finish
+        self.output = {'path': self.path, 'mime': self.mime, 'DateTimeOriginal': self._get_iso_time(self.tags['EXIF DateTimeOriginal'])}
+        pass
+
+    def _get_iso_time(self, exif_time):
+        """ get (convert) to ISO 8601 time format """
+        xtime = exif_time.values
+        year = xtime[0:4]
+        month = xtime[5:7]
+        day = xtime[8:10]
+        hour = xtime[11:13]
+        minute = xtime[14:16]
+        second = xtime[17:19]
+        return year+'-'+month+'-'+day+'T'+hour+':'+minute+':'+second
+
+    def _get_exif_tags(self, file_path):
+        """ open file, read exif meta data """
+        tags = None
+        try:
+            f = io.open( file_path, 'rb')
+            tags = exifread.process_file(f)
+            f.close()
+        except IOError:
+            self.error = {'path': file_path, 'error': repr(sys.exc_info()[1])}
+        finally:
+            return tags
 
     def _get_header_bytes(self, file_path):
         """ open file, read header bytes, close file """
